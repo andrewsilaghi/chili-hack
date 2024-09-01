3a5c815405c1ea33304c0e467016d7e4ef5b2bdd
Andrew Silaghi <silaghiandrew89@gmail.com>
2024-09-01 18:37:57 -0500

    footer + full faq & nav linked to sections

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -21,3 +21,6 @@
 import "../../node_modules/bootstrap/js/dist/util.js";
 import "../../node_modules/bootstrap/js/dist/modal.js";
 import "../../node_modules/bootstrap/js/dist/collapse.js"
+
+
+// element.scrollIntoView({ behavior: "smooth", inline: "nearest" });
